e20911c086bec1e56e1005c59215d538413527d1
agohorel <alex.oddlogic@gmail.com>
2018-12-20 22:47:12 -0500

    refactored whole approach, add logging to json

--- parse.js
+++ parse.js
@@ -1,16 +1,17 @@
-const request = require('request');
-const colors = require('colors');
+const fs = require("fs");
+const request = require("request");
+const colors = require("colors");
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
-let dom;
 
 // max no. of items to lookup (each page has 20 results)
 // current max is set really low to not be spammy
-let max = 420;
+let max = 200;
 let urlList = [];
+let dom;
 
 // insert pages to request until max
-for (var i = 400; i < max; i+=20){
+for (var i = 0; i < max; i+=20){
 	let url = `https://www.kvraudio.com/plugins/windows/macosx/instruments/effects/hosts/free/newest/start${i}`;
 	urlList.push(url);
 }
@@ -41,94 +42,48 @@ urlList.forEach((url, i) => {
 
 				// convert raw body text to dom
 				dom = new JSDOM(body);
-				// pass dom to constructURLs() to parse 
-				constructURLs(dom);
+				// pass dom to getTags() to parse 
+				getTags(dom);
 			}
 		});
-	}, 5000 * i);
+	}, 10000 * i);
 });
 
-function constructURLs(dom){
+function getTags(dom){
 	// get node list containing all product boxes on each page
-	let productBoxes = dom.window.document.querySelectorAll(".kvrpboximg");
-	let disallowedCharacters = ["{", "}", "|", "\\", "^", "~", "[", "]", "`", " ", ":", "."];
-	let productUrlList = [];
+	let productBoxes = dom.window.document.querySelectorAll(".kvrpboxa");
+	let vsts = {};
 
 	productBoxes.forEach((product) => {
-		// get the child nodes of each product box
-		let children = product.nextElementSibling.childNodes;
-		// break off strings from product's child nodes to get names
-		let productName = children[0].textContent;
-		let authorName = children[1].childNodes[2].data;
-
-		// replace disallowed characters with hyphens
-		disallowedCharacters.forEach((character) => {
-			productName = findAndReplace(productName, character, "-");
-			authorName = findAndReplace(authorName, character, "-");
-		});
-
-		// handle case where 2 disallowed characters occur back to back
-		productName = findAndReplace(productName, "--", "-");
-		authorName = findAndReplace(authorName, "--", "-");
-
-		// handle possesive s's 
-		productName = findAndReplace(productName, "'", "");
-		authorName = findAndReplace(authorName, "'", "");
-
-		// concatenate (hopefully) valid URLs
-		let url = `https://www.kvraudio.com/product/${productName}${authorName}`;
+		// get product info
+		let url = product.href;
+		let name = product.childNodes[1].childNodes[1].childNodes[0].data;
+		let author = product.childNodes[1].childNodes[1].childNodes[2].data;
+		// trim off " by "
+		author = author.substring(4, author.length);
 		
-		productUrlList.push(url);
+		vsts[product] = {
+			url, name, author
+		};
 	});
-	console.log(productUrlList);
-
-	testURLs(productUrlList);
+	saveResults(vsts);
 }
 
-function findAndReplace(string, target, replacement){
-	for (let i = 0; i < string.length; i++) {
-		string = string.replace(target, replacement);
-	}
-
-	return string;
-}
-
-function testURLs(urlArray){
-	let passed = 0;
-	let failed = 0;
-	urlArray.forEach((url, i) => {
-	// 1 sec. cooldown to prevent spamminess
-	setTimeout(() => {
-		request({ 
-			uri: url,
-	        followRedirect: (response) => {
-	            console.log(`Provided URL ${url} was redirected to: ${response.headers.location}.`.red);
-	            failed++;
-	            return true;
-	        }
-    	}, (error, response, body) => {
-				if (error){
-					// print error in red if request fails outright
-					console.log(`received ${error} on ${url}`.red);
-				} else {
-					let status = response.statusCode;
-					// print green if OK, red if any sort of error
-					if (status >= 200 && status < 300){
-						console.log(`received status code ${status} on ${url}`.green);
-						passed++;
-					} else {
-						console.log(`received status code ${status} on ${url}`.red);
-						failed++;
-					}
-				}
+function saveResults(vsts){
+	let path = "./vsts.json";
+	// check if file exists
+	fs.stat(path, (err, stats) => {
+		// if not, write file
+		if (err || stats === undefined){			
+			fs.writeFile(path, JSON.stringify(vsts, null, 2), (err) => {
+				if (err) throw err;
+				else console.log("saved results to disk.");
+			});		
+		} else {
+			fs.appendFile(path, JSON.stringify(vsts, null, 2), (err) => {
+				if (err) throw err;
+				else console.log("appended results to disk.");
 			});
-		}, 1000 * i);
+		}
 	});
-
-	// @TODO fix this nonsense w/ async/await
-	setTimeout(() => {
-		console.log(`done checking this batch of URLs. ${passed} URLs passed and ${failed} URLs failed.`);
-	}, 1000 * urlArray.length)		
 }
-
-
